dc5a0ad9b88ba5845301439dac3e91580b25a877
nurrizqifp <nurrizqifebriansyah@gmail.com>
2025-05-06 14:59:20 +0700

    Membuat class Lingkaran

--- contoh.cpp
+++ contoh.cpp
@@ -20,4 +20,23 @@ class PersegiPanjang
         {
             return a * b;
         }
+
+        Lingkaran ol;
+};
+
+class Lingkaran
+{
+    public:
+        int jarijari;
+
+        void input()
+        {
+            cout << "Masukkan jari-jari : ";
+            cin >> jarijari;
+        }
+
+        int luasLingkaran(int r)
+        {
+            return 3.14 * r * r;
+        }
 };
